76174d7f601f4d139190027fc3be81901de1e853
M. Imam Pratama <imambungo@gmail.com>
2019-11-18 10:26:35 +0700

    siapkan method _taskListBuilder

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../task.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,12 +7,45 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<StatefulWidget> {
+  List<Task> tasks = [
+    Task(task: 'jogging', hariH: -3, periode: 7),
+    Task(task: 'main voli', hariH: 1, periode: 2),
+    Task(task: 'belajar mobile app', hariH: 2, periode: 4),
+  ];
+
+  Widget _taskListBuilder() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: tasks.length,
+      itemBuilder: (context, index) {
+        return _taskBuilder(tasks[index]);
+      },
+    );
+  }
+
+  Widget _taskBuilder(Task task) {
+    return ListTile(
+      title: Text(
+        task.task,
+      ),
+      leading: Text(
+        '${task.periode}',
+      ),
+      trailing: Text(
+        '${task.hariH}',
+      ),
+      onTap: () {
+        // TODO
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
       body: SafeArea(
-        child: Text('nah'),
+        child: _taskListBuilder(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
